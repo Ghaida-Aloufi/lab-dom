78753b6eedfa3842c01b30baf3585c450685121a
Ghaida-Aloufi <ghaida2219@gmail.com>
2024-09-12 14:33:41 +0300

    add lab

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let x = document.getElementsByTagName("button")[0]
 let y = document.getElementsByTagName("button")[1]
 let z = document.getElementsByTagName("button")[2]
+
+
+
 x.classList.add('button');
 y.classList.add('button');
 z.classList.add('button');
